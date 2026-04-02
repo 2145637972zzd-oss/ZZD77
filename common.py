9a2916c22efa9c4e7fecf22daceb18b126ebe323
2145637972zzd-oss <2145637972zzd@gmail.com>
2026-04-02 15:32:34 +0800

    Delete common.py

--- common.py
+++
@@ -1,8 +0,0 @@
-# app/utils/common.py
-from flask import jsonify
-
-def success_response(data=None, msg="成功", code=200):
-    return jsonify({"code": code, "msg": msg, "data": data}), code
-
-def error_response(msg="失败", code=400, data=None):
-    return jsonify({"code": code, "msg": msg, "data": data}), code
