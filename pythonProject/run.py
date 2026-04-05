88a33984e085ff4cb56606fae94c6b8cd4cf2712
2145637972zzd-oss <2145637972zzd@gmail.com>
2026-04-05 12:36:48 +0800

    Delete pythonProject directory

--- pythonProject/run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app, db
-
-app = create_app()
-
-if __name__ == '__main__':
-    with app.app_context():
-        # 注意：生产环境中通常不建议用 create_all，而是用 Flask-Migrate，但毕设用这个没问题
-        db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
